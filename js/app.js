6f989505b6f54cf6f953c1f41598dd645f6b513e
dev-Barboza <matheus.barboza.henrique@gmail.com>
2022-02-14 19:35:33 -0300

    fineshed responsive

--- js/app.js
+++ js/app.js
@@ -1,30 +1,32 @@
 //ScroolPage
-const menuItems = document.querySelectorAll('.navbar a[href^="#"]');
-//Verifica de foi clicado 
-menuItems.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick);
-  })
-  
-  function getScrollTopByHref(element) {
-    const id = element.getAttribute('href');
-    return document.querySelector(id).offsetTop;
-  }
-  
-  function scrollToIdOnClick(event) {
-    event.preventDefault();
-    const to = getScrollTopByHref(event.target) - 80;
-    scrollToPosition(to);
-  }
-  
-  function scrollToPosition(to) {
-    // window.scroll({
-    //   top: to,
-    //   behavior: "smooth",
-    // });
-    smoothScrollTo(0, to);
-  }
-  
+const menuItens = document.querySelectorAll('.navbar a[href^="#"]');
+
+menuItens.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.target) - 80;
+  scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+  smoothScrollTo(0, to);
+}
+
+
 //Modal
+
 var modalBtn = document.querySelector('.butoes');
 var modalBg =document.querySelector('.modalBg');
 var modalClose = document.querySelector('.modalClose');
@@ -37,3 +39,38 @@ modalClose.addEventListener('click', function(){
   modalBg.classList.remove('bg-active');
 });
 
+
+///Conteudo Modal
+
+function evento() {
+  document.getElementById("buttonEventos").click();
+  document.getElementById('resposta').innerHTML = 'Bem vindo(a) á seção de eventos ';
+};
+
+function usuario() {
+  document.getElementById("buttonUsuarios").click();
+  document.getElementById('resposta').innerHTML = 'Bem vindo(a) á seção de usuários !'
+}
+
+function relatorio() {
+  document.getElementById("buttonRelatorios").click();
+  document.getElementById('resposta').innerHTML = 'Bem vindo(a) á seção de relatórios !'
+}
+
+const buttons = document.querySelectorAll('.individualButton');
+buttons.forEach(btn => {
+  btn.addEventListener('click', function(e){
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+  })
+})
+
+//Responsividade
+const navbar = document.querySelector('.navbar');
+const button = document.querySelector('.mobileButton');
+const closeButton = document.querySelector('.mobileButton').children[0];
+
+button.addEventListener('click', () => {
+  navbar.classList.toggle('active')
+  closeButton.classList.toggle(".navbar a")
+}) 
